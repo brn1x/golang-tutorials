f6f481aedf7a3b39b7b138ca8a1b855cb382dd96
Bruno Almeida <brunokfsop@gmail.com>
2023-01-19 13:45:19 -0300

    feat: third tutorial step: slices, random and init function

--- creating-module/greetings/greetings.go
+++ creating-module/greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
@@ -10,6 +12,21 @@ func Hello(name string) (string, error) {
 		return "", errors.New("name param can't be empty")
 	}
 
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formmats := []string{
+		"Hi, %v, Welcome!",
+		"Great to see you, %v",
+		"Hail, %v! Well met!",
+		"Yoo %v, good to see u bro!",
+	}
+
+	return formmats[rand.Intn(len(formmats))]
 }
